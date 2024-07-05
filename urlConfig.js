cf5c19dcd543ca9be8fe09748cd52400d9956445
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 23:48:15 +0530

    Delete urlConfig.js

--- urlConfig.js
+++
@@ -1,5 +0,0 @@
-const FRONTEND_URL =
-  process.env.NODE_ENV === "development"
-    ? `http://localhost:3000`
-    : `https://process-visualizer-frontend.vercel.app`;
-module.exports = FRONTEND_URL;
