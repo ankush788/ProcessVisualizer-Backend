305555d3a7321079f692d9a567004b673d9e7c56
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 17:46:06 +0530

    Update urlConfig.js

--- urlConfig.js
+++ urlConfig.js
@@ -1,5 +1,5 @@
 const FRONTEND_URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:3000`
-    : `https://process-visualizer-frontend.vercel.app/`;
+    : `https://process-visualizer-frontend.vercel.app`;
 module.exports = FRONTEND_URL;
