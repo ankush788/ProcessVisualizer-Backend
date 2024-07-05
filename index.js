20976fa25ffbf5577dc2fe0cda9a8eaf06546dfb
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 19:31:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
 const express = require("express");
-var cors = require("cors");
+const cors = require("cors");
 const fcfsRouter = require("./Routes/fcfsRoute");
 const sjfRouter = require("./Routes/sjfRoute");
 const roundRobinRouter = require("./Routes/rrRoute");
-const priorityRouter = require("./Routes/priorityRoute");  //  priority low to high 
-const algoCompareRouter = require("./Routes/algoCompareRoute"); // using this require both things in data timeQuantum , priority 
+const priorityRouter = require("./Routes/priorityRoute");  // priority low to high 
+const algoCompareRouter = require("./Routes/algoCompareRoute"); // using this require both things in data timeQuantum, priority 
 const FRONTEND_URL = require("./urlConfig");
-//  const connectToDatabase = require("./Config/database");
-//  connectToDatabase();
+// const connectToDatabase = require("./Config/database");
+// connectToDatabase();
 
 const app = express();
-app.use(cors());
 const port = process.env.PORT || 5000;
+
 app.use(express.json());
 
-// app.set("trust proxy", 1);
-// app.use(
-//   cors({
-//     origin: `${FRONTEND_URL}`,
-//     credentials: true,
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
+app.set("trust proxy", 1);
+
+// Use specific CORS configuration
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 app.get("/", (req, res) => {
-  res.send("We are live ");
+  res.send("We are live");
 });
 
-app.use("/fcfs" , fcfsRouter);
-app.use("/sjf",  sjfRouter);
+app.use("/fcfs", fcfsRouter);
+app.use("/sjf", sjfRouter);
 app.use("/rr", roundRobinRouter);
 app.use("/priority", priorityRouter);
 app.use("/compare", algoCompareRouter);
 
 app.use((req, res) => {
   res.status(404).send("Wrong path");
-})
+});
 
 app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port}`);
