87de45a7c72c7a6fdf0d745a4797056d74ccfef2
ANKUSH SINGH <ankushsingh_se21a13_54@dtu.ac.in>
2024-07-05 20:28:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,13 @@ const fcfsRouter = require("./Routes/fcfsRoute");
 const sjfRouter = require("./Routes/sjfRoute");
 const roundRobinRouter = require("./Routes/rrRoute");
 const priorityRouter = require("./Routes/priorityRoute");  //  priority low to high 
-const algoCompareRouter = require("./Routes/algoCompareRoute"); // using this require both things in data timeQuantum , priority 
 //  const connectToDatabase = require("./Config/database");
 //  connectToDatabase();
-const FRONTEND_URL = require("./urlConfig");
+
 const app = express();
+app.use(cors());
 const port = process.env.PORT || 5000;
 app.use(express.json());
-
-app.set("trust proxy", 1);
-app.use(
-  cors({
-    origin: `${FRONTEND_URL}`,
-    credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Set-Cookie"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
 
 app.get("/", (req, res) => {
   res.send("We are live ");
@@ -30,9 +19,7 @@ app.get("/", (req, res) => {
 app.use("/fcfs" , fcfsRouter);
 app.use("/sjf",  sjfRouter);
 app.use("/rr", roundRobinRouter);
-app.use("/priority", priorityRouter);
-app.use("/compare", algoCompareRouter);
-
+app.use("/priority", priorityRouter)
 app.use((req, res) => {
   res.status(404).send("Wrong path");
 })
